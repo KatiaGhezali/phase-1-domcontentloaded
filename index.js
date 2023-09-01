20f96b63fddb1280779e549606328b26214312c4
Katia Ghezali <ghezalikatia@hotmail.com>
2023-09-01 17:20:01 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+  const txt = document.getElementById('text');
+  txt.innerHTML = "This is really cool!"
+
+});
+
